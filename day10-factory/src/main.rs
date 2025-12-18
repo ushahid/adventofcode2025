063de9404dcd7c86656c22e00453b2ab0d6cf747
Usman Shahid <usman.shaahid@gmail.com>
2025-12-18 17:45:28 -0600

    Added solution for day 10 part 1

---
+++ day10-factory/src/main.rs
@@ -0,0 +1,77 @@
+use std::fs;
+use std::collections::{HashSet, VecDeque};
+use regex::Regex;
+
+
+
+fn switches_to_u64(switches: &Vec<u64>) -> u64 {
+    let mut bin_switches: u64 = 0;
+    for switch in switches {
+        let to_or: u64 = 1 << switch;
+        bin_switches |= to_or;
+    }
+    return bin_switches;
+}
+
+
+fn main() {
+    let data = fs::read_to_string("input.txt").expect("Could not read file...");
+    let switches_regex = Regex::new(r"\((?<switches>([0-9]+,?)+)\)").unwrap();
+    let target_regex = Regex::new(r"\[(?<target>[\.#]+)\]").unwrap();
+    let joltage_regex = Regex::new(r"\{(?<joltage>([0-9]+,?)+)\}").unwrap();
+
+    let mut all_targets: Vec<Vec<char>> = Vec::new();
+    let mut all_switches: Vec<Vec<Vec<u64>>> = Vec::new();
+    let mut all_joltages: Vec<Vec<u64>> = Vec::new();
+
+    for line in data.lines() {
+        let Some(c) = target_regex.captures(line) else { panic!("Parsing error for Target"); };
+        let target: Vec<char> = c.name("target").unwrap().as_str().chars().collect();
+        all_targets.push(target);
+        let mut switches: Vec<Vec<u64>> = Vec::new();
+        for s in switches_regex.captures_iter(line) {
+            switches.push(s.name("switches").unwrap().as_str().split(",").map(|x| x.parse::<u64>().unwrap()).collect());
+        }
+        all_switches.push(switches);
+        let Some(c) = joltage_regex.captures(line) else { panic!("Parsing error for Joltage"); };
+        let joltage: Vec<u64> = c.name("joltage").unwrap().as_str().split(",").map(|x| x.parse::<u64>().unwrap()).collect();
+        all_joltages.push(joltage)
+    }
+
+    let bin_targets: Vec<u64> = all_targets
+                                    .iter()
+                                    .map(|c: &Vec<char>| c.iter()
+                                                            .enumerate()
+                                                            .filter(|(_, c)| **c == '#')
+                                                            .map(|(index, _)| index as u64)
+                                                            .collect::<Vec<u64>>())
+                                    .map(|switches| switches_to_u64(&switches))
+                                    .collect();
+    let bin_switches: Vec<Vec<u64>> = all_switches
+                                        .iter()
+                                        .map(|x| {
+                                            x.iter().map(|switches| switches_to_u64(switches)).collect()
+                                        }).collect();
+    
+    // Part 1 solution
+    let mut total = 0;
+    for i in 0..bin_targets.len() {
+        let mut seen: HashSet<u64> = HashSet::new();
+        let mut queue: VecDeque<(u64, u64)> = VecDeque::new();
+        queue.push_back((0, 0));
+        while let Some((state, depth)) = queue.pop_front() {
+            if state == bin_targets[i] {
+                    total += depth;
+                    break;
+            }
+            if !seen.contains(&state) {
+                seen.insert(state);
+                for switch in bin_switches[i].iter() {
+                    let new_state = (*switch) ^ state;
+                    queue.push_back((new_state, depth + 1));
+                }
+            }
+        }
+    }
+    println!("Part 1 answer: {}", total);
+}
